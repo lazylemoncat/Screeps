a0e07e4fc7858c45892d0e4b4b691cd1f9c78c68
lazylemoncat <wenzhao5105@163.com>
2023-01-21 18:59:02 +0800

    2023/1/21 18:58

--- src/helper/helper.ts
+++ src/helper/helper.ts
@@ -1,3 +1,32 @@
-global.hlper = function() {
-  console.log()
+import { tasks } from "@/Tasks/Tasks";
+
+global.helper = function(text: string) {
+  switch (text) {
+    case 'controller': {
+      for (let name in Memory.rooms) {
+        let room = Memory.rooms[name];
+        let controller = Game.getObjectById(room.controller);
+        let progress = controller.progress / controller.progressTotal;
+        console.log(name);
+        console.log(controller.level);
+        console.log('升级还需要', controller.progressTotal - controller.progress,  progress);
+        console.log('可用安全模式次数为：', controller.safeModeAvailable);
+        console.log('-------------------------------------------------------');
+      }
+      break;
+    }
+    case 'tasks': {
+      console.log('withdrawTasks:');
+      for (let i = 0; i < tasks.withdraw.length; ++i) {
+        console.log(tasks.withdraw[i]);
+      }
+      console.log('transferTasks:');
+      for (let i = 0; i < tasks.transfer.length; ++i) {
+        console.log(tasks.transfer[i]);
+      }
+      console.log('-------------------------------------------------------');
+      break;
+    }
+  }
+  
 }
